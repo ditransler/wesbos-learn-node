691ce752d8582c3eb9967bdcc3e358d79447ad9a
Dmitriy Beglov <ditransler@gmail.com>
2017-10-29 20:23:42 +0400

    Finished setting up and Core Concept - Routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(wes);
+  // res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;

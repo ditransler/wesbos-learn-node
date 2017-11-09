6f891767d53dca836d8737db3c08f956ab9f7f7b
Dmitriy Beglov <ditransler@gmail.com>
2017-11-09 21:44:15 +0400

    Finished Control Flow - Flash Messages

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -11,7 +11,7 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };

885d0948506367386884059c12f0ba4dc9f012a9
Dmitriy Beglov <ditransler@gmail.com>
2017-11-09 21:34:27 +0400

    Finished Control Flow - Using Async Await

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const stroreController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', stroreController.homePage);
 router.get('/add', stroreController.addStore);
-router.post('/add', stroreController.createStore);
+router.post('/add', catchErrors(stroreController.createStore));
 
 module.exports = router;

e57727bd01fedbf09aa65ac62191515038984d24
Dmitriy Beglov <ditransler@gmail.com>
2017-11-22 22:51:13 +0400

    Finished Control Flow - Creating and Editing Flow for Stores

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -20,3 +20,21 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  // TODO: 2. confirm they are the owner of the store
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  res.redirect(`/stores/${store._id}/edit`)
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const stroreController = require('../controllers/storeController');
+const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', catchErrors(stroreController.getStores));
-router.get('/stores', catchErrors(stroreController.getStores));
-router.get('/add', stroreController.addStore);
-router.post('/add', catchErrors(stroreController.createStore));
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get(`/stores/:id/edit`, catchErrors(storeController.editStore));
 
 module.exports = router;

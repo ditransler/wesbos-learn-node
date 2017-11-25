d2790d8d0eff5544e47d03287a3da1d8fa9d020d
Dmitriy Beglov <ditransler@gmail.com>
2017-11-25 15:21:11 +0400

    Finished Geolocation - Quick Data Visualization Tip

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -30,6 +30,9 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = 'Point';
+
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return the new store instead of the old one
     runValidators: true

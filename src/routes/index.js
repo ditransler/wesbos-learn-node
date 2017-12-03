413d681b4b3c8cbc8d292806a1f067214ab7696d
Dmitriy Beglov <ditransler@gmail.com>
2017-12-03 16:58:03 +0400

    Finished User Accounts and Authentication - Creating a User Account Edit Screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -42,4 +42,11 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account',
+  authController.isLoggedIn,
+  userController.account
+);
+
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
